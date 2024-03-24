146c9c9e252a95193a926496a8608b081a1ae987
MariamAziz0 <ma27523@gmail.com>
2024-03-24 22:22:58 +0200

    Fixing kmeans and implementing Evaluator

---
+++ src/Evaluator.py
@@ -0,0 +1,71 @@
+import numpy as np
+import math
+
+
+class Evaluator:
+    def __init__(self, true_labels, clusters):
+        self.true_labels = true_labels
+        self.clusters = clusters
+        self.true_positive = 0.0
+        self.true_negative = 0.0
+        self.false_positive = 0.0
+        self.false_negative = 0.0
+        self.confusion_matrix = np.zeros((len(clusters), np.max(true_labels) + 1))
+        self.compute()
+
+    def compute(self):
+        for i in range(len(self.clusters)):
+            for j in range(len(self.clusters[i])):
+                self.confusion_matrix[i, self.true_labels[self.clusters[i][j]]] += 1
+
+        for i in range(self.confusion_matrix.shape[0]):
+            for j in range(self.confusion_matrix.shape[1]):
+                if self.confusion_matrix[i, j] > 1:
+                    self.true_positive += (self.confusion_matrix[i, j] * (self.confusion_matrix[i, j] - 1)) / 2
+
+                for k in range(j + 1, self.confusion_matrix.shape[1]):
+                    self.false_positive += self.confusion_matrix[i, j] * self.confusion_matrix[i, k]
+
+        for j in range(self.confusion_matrix.shape[1]):
+            for i in range(self.confusion_matrix.shape[0]):
+                for k in range(i + 1, self.confusion_matrix.shape[0]):
+                    self.false_negative += self.confusion_matrix[i, j] * self.confusion_matrix[k, j]
+
+                    for m in range(self.confusion_matrix.shape[1]):
+                        if m != j:
+                            self.true_negative += self.confusion_matrix[i, j] * self.confusion_matrix[k, m]
+
+    def compute_precision(self):
+        if self.true_positive + self.false_positive == 0.0:
+            return 0.0
+
+        return self.true_positive / (self.true_positive + self.false_positive)
+
+    def compute_recall(self):
+        if self.true_positive + self.false_negative == 0.0:
+            return 0.0
+
+        return self.true_positive / (self.true_positive + self.false_negative)
+
+    def compute_f1(self):
+        precision = self.compute_precision()
+        recall = self.compute_recall()
+
+        if precision + recall == 0.0:
+            return 0.0
+
+        return 2 * precision * recall / (precision + recall)
+
+    def compute_conditional_entropy(self):
+        conditional_entropy = 0.0
+
+        for i in range(self.confusion_matrix.shape[0]):
+            n_elements_in_cluster = sum(self.confusion_matrix[i, :])
+
+            for j in range(self.confusion_matrix.shape[1]):
+                if self.confusion_matrix[i, j] != 0:
+
+                    conditional_entropy -= ((self.confusion_matrix[i, j] / n_elements_in_cluster) *
+                                            math.log2(self.confusion_matrix[i, j] / n_elements_in_cluster))
+
+        return conditional_entropy
--- src/Kmeans.py
+++ src/Kmeans.py
@@ -6,14 +6,11 @@ class KmeansClustering:
     def __init__(self, k=8, max_iterations=500, random_state=None):
         self.clusters = None
         self.centroids = None
-        self.cluster_representative = None
-        self.true_labels = None
-        self.assigned_labels = None
         self.k = k
         self.max_iterations = max_iterations
         self.random_state = random_state
 
-    def fit(self, X, true_labels):
+    def fit(self, X):
         np.random.seed(self.random_state)
         self.centroids = X[np.random.choice(range(len(X)), size=self.k, replace=False)]
         new_assignment = None
@@ -28,10 +25,9 @@ class KmeansClustering:
 
             self.centroids = new_centroids
 
-        self._partition_clusters(true_labels, new_assignment)
-        self.assigned_labels = self._assign_label_to_points(new_assignment)
+        self._partition_clusters(new_assignment)
 
-        return self.assigned_labels
+        return self.clusters
 
     def _assignment(self, X):
         centroids_with_new_axis = self.centroids[:, np.newaxis]
@@ -42,22 +38,14 @@ class KmeansClustering:
     def _compute_new_centroids(self, X, new_assignment):
         return np.array([X[new_assignment == i].mean(axis=0) for i in range(self.k)])
 
-    def _partition_clusters(self, true_labels, assignment):
+    def _partition_clusters(self, assignment):
         self.clusters = [[] for _ in range(self.k)]
-        for i in range(len(true_labels)):
-            self.clusters[assignment[i]].append(true_labels[i])
-
-        self._compute_cluster_representative()
-
-    def _compute_cluster_representative(self):
-        self.cluster_representative = [max(element, key=element.count) for element in self.clusters]
-
-    def _assign_label_to_points(self, assignment):
-        return np.array([self.cluster_representative[i] for i in assignment])
+        for i in range(len(assignment)):
+            self.clusters[assignment[i]].append(i)
 
     def predict(self, X):
         centroids_with_new_axis = self.centroids[:, np.newaxis]
         difference_points_centroids = X - centroids_with_new_axis
         distances = np.sqrt((difference_points_centroids ** 2).sum(axis=2))
         assignment = np.argmin(distances, axis=0)
-        return self._assign_label_to_points(assignment)
+        return assignment
