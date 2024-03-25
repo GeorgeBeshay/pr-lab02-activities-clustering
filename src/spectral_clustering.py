77fdbc1787217a9e9c74a018f46402a933214d83
George Beshay <georgebeshay101@gmail.com>
2024-03-25 17:15:50 +0200

    Fixed spectral clustering alg.

--- src/spectral_clustering.py
+++ src/spectral_clustering.py
@@ -4,37 +4,44 @@ import math
 from scipy.linalg import norm
 from sklearn.cluster import KMeans
 from Kmeans import KmeansClustering
-
+from kernel_functions import *
 
 class SpectralClustering:
-    def __init__(self, kernel_function="nn", n_clusters=19):
-        self.C = None
+    def __init__(self, kernel_function=compute_nn_similarity, n_clusters=19):
         self.Y = None
+        self.eigenvectors = None
         self.eigenvalues = None
         self.kernel_function = kernel_function
         self.n_clusters = n_clusters
 
     def fit(self, D):
-        A = compute_nn_similarity(D)
+        A = self.kernel_function(D)
 
         delta = np.diag(np.sum(A, axis=1))
         assert delta.shape == A.shape, "Degree matrix shape does not match the Data matrix shape."
 
         # Compute B = L_a = I - delta^-1 . A
-        B = np.identity(D.shape[0]) - (np.linalg.inv(delta) @ A)
+        delta_inv = np.diag(1 / np.diagonal(delta))
+        B = np.identity(D.shape[0]) - (delta_inv @ A)
 
         unsorted_eigenvalues, unsorted_eigenvectors = np.linalg.eig(B)
         sorted_indices = np.argsort(unsorted_eigenvalues)
-        eigenvalues = unsorted_eigenvalues[sorted_indices]
-        eigenvectors = unsorted_eigenvectors[:, sorted_indices]
-        U = eigenvectors[:, :self.n_clusters]
+        eigenvalues = np.real(unsorted_eigenvalues[sorted_indices])
+        eigenvectors = np.real(unsorted_eigenvectors[:, sorted_indices])
 
-        Y = U / norm(U, axis=1, keepdims=True)
-        print(U.shape, Y.shape)
+        constant_eigenvectors_count = np.count_nonzero(eigenvalues < 1e-12)
+        selected_indices = eigenvalues > 1e-12
+        selected_eigenvalues = eigenvalues[selected_indices]
+        selected_eigenvectors = eigenvectors[selected_indices]
+
+        U = selected_eigenvectors[:, :self.n_clusters - 1]
+        Y = U / (norm(U, axis=1, keepdims=True) + 1e-10)
+
         assert Y.shape == U.shape, "Y matrix shape does not match the U matrix shape."
 
         # Store the results to the object fields
-        self.eigenvalues = eigenvalues
+        self.eigenvalues = selected_eigenvalues
+        self.eigenvectors = selected_eigenvectors
         self.Y = Y
 
     def predict(self):
